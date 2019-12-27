324897dc33112efbdacac25f21072eece15ed975
Gaurav Arora <gauravarora.daiict@gmail.com>
2019-12-27 19:04:51 +1100

    Ugly

---
+++ isUgly.cpp
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/ugly-number/
+
+class Solution {
+public:
+    bool isUgly(int num) {
+        bool ugly = true;
+        if (num == 0) return false;
+        while(true) {
+            if ((num % 5) == 0) {
+                num = num/5;
+            } else if ((num%3) == 0) {
+                num = num/3;
+            } else if ((num%2) == 0) {
+                num = num/2;
+            } else {
+                if (num != 1) {
+                    ugly = false;
+                }
+                break;
+            }
+        }
+        return ugly;
+    }
+};
