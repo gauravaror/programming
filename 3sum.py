1721a3349287b70948b07ed029e4c01600a518c1
Gaurav Arora <gauravarora.daiict@gmail.com>
2020-04-08 20:22:59 +1000

    Started 3sum, will finish it today

---
+++ 3sum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], exclude: int, equal:int):
+        hh = {}
+        for idx,i in enumerate(nums):
+            if idx == exclude:
+                continue
+            
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+
