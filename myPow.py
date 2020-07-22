7f630c588d4823489c3c2c13b592b79101414136
Gaurav Arora <gauravarora.daiict@gmail.com>
2020-07-23 00:31:33 +1000

    My power

---
+++ myPow.py
@@ -0,0 +1,13 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if n == 0:
+            return 1
+        if n == 1:
+            return x
+        if n < 0:
+            x = 1/x
+            n = -n
+        if n%2:
+            return x*self.myPow(x, n-1)
+        g = self.myPow(x, n//2)
+        return g*g
