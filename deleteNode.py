278aa9d1ddc4729be81fe607aa4ae395aa2e2647
Gaurav Arora <gauravarora.daiict@gmail.com>
2020-07-23 22:36:21 +1000

    deleteNode

---
+++ deleteNode.py
@@ -0,0 +1,15 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        nextone = node.next
+        node.val = nextone.val
+        node.next = nextone.next
