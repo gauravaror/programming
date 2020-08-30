5b1b8ea44108a684098040cc49d057cf09c8ba04
Gaurav Arora <gauravarora.daiict@gmail.com>
2020-08-30 23:23:32 +1000

    UnionFind Classic Example

---
+++ largestComponent.py
@@ -0,0 +1,42 @@
+class Solution:
+    def largestComponentSize(self, A: List[int]) -> int:
+        self.maxelem = max(A)
+        self.parent = [i for i in range(self.maxelem+1)]
+        self.size = [0]*(self.maxelem + 1)
+        for i in A:
+            self.size[i] = 1
+    
+        for elem in A:
+            for factor in range(2, int(sqrt(elem)) + 1):
+                if elem % factor == 0:
+                    self.union(elem, factor)
+                    self.union(elem, elem//factor)
+                    #print(self.size, elem, factor, elem//factor, self.parent)
+        groups = defaultdict(int)
+        maxsize = 0
+        #print(self.parent)
+        #print(self.size)
+        for elem in A:
+            group_id = self.find(elem)
+            #print(elem, group_id)
+            groups[group_id] = self.size[group_id]
+            if groups[group_id] > maxsize:
+                maxsize = groups[group_id]
+        return maxsize
+                    
+            
+            
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] =  self.find(self.parent[x])
+        return self.parent[x]
+    
+    def union(self, x, y):
+        px,py = self.find(x), self.find(y)
+        if px == py:
+            return px
+        if self.size[px] > self.size[py]:
+            px,py = py,px
+        self.parent[px] = py
+        self.size[py] += self.size[px]
+        return py
